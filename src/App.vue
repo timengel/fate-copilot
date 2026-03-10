28d099c2c3aa99c7e0cfc98e53ea610924283ab0
Tim Engel <tim.engel.lp@gmail.com>
2026-03-10 15:14:57 +0100

    ref: initial responsive design

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
-import { RouterView, RouterLink } from 'vue-router'
+import { ref, watch } from 'vue'
+import { RouterView, RouterLink, useRouter } from 'vue-router'
 import ImportExportBar from './components/shared/ImportExportBar.vue'
 import { initPersistence } from './composables/usePersistence'
 
 initPersistence()
+
+const navOpen = ref(false)
+const router = useRouter()
+watch(() => router.currentRoute.value.path, () => { navOpen.value = false })
 </script>
 
 <template>
@@ -13,13 +18,27 @@ initPersistence()
         <RouterLink to="/" class="nav-logo">
           <span class="fate-plus">+</span>FATE
         </RouterLink>
-        <div class="nav-links">
-          <RouterLink to="/dashboard" class="nav-link">Dashboard</RouterLink>
-          <RouterLink to="/campaigns" class="nav-link">Kampagnen</RouterLink>
-          <RouterLink to="/characters" class="nav-link">Charaktere</RouterLink>
-          <RouterLink to="/skills" class="nav-link">Fertigkeiten</RouterLink>
+
+        <button
+          class="nav-hamburger"
+          :aria-expanded="navOpen"
+          aria-label="Navigation öffnen"
+          @click="navOpen = !navOpen"
+        >
+          <span class="hamburger-bar"></span>
+          <span class="hamburger-bar"></span>
+          <span class="hamburger-bar"></span>
+        </button>
+
+        <div class="nav-drawer" :class="{ 'nav-open': navOpen }">
+          <div class="nav-links">
+            <RouterLink to="/dashboard" class="nav-link">Dashboard</RouterLink>
+            <RouterLink to="/campaigns" class="nav-link">Kampagnen</RouterLink>
+            <RouterLink to="/characters" class="nav-link">Charaktere</RouterLink>
+            <RouterLink to="/skills" class="nav-link">Fertigkeiten</RouterLink>
+          </div>
+          <ImportExportBar />
         </div>
-        <ImportExportBar />
       </nav>
     </header>
 
